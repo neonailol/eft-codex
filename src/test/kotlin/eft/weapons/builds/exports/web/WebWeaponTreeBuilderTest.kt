09363d3cdc1e1d3384244ebffd9af2fd1c5c9936
Neonailol <neonailol@gmail.com>
2019-09-30 18:32:36 +0300

    All weapons can be processed and exported
    Signed-off-by: Neonailol <neonailol@gmail.com>

--- src/test/kotlin/eft/weapons/builds/exports/web/WebWeaponTreeBuilderTest.kt
+++ src/test/kotlin/eft/weapons/builds/exports/web/WebWeaponTreeBuilderTest.kt
@@ -2,9 +2,13 @@ package eft.weapons.builds.exports.web
 
 import eft.weapons.builds.tree.weaponBuilds
 import eft.weapons.builds.utils.Items
+import eft.weapons.builds.utils.Locale.itemName
+import eft.weapons.builds.utils.Locale.itemShortName
 import eft.weapons.builds.utils.printJson
 import eft.weapons.builds.utils.stringBuilder
 import org.testng.annotations.Test
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class WebWeaponTreeBuilderTest {
 
@@ -21,15 +25,22 @@ class WebWeaponTreeBuilderTest {
 
     @Test
     fun `can export all weapons`() {
+        val tempDir = createTempDir()
         val weaponTypes = Items.all().filter { it.parent == "5422acb9af1c889c16000029" }
         for (weaponType in weaponTypes) {
-            println("Exporting ${weaponType.name}")
+            val categoryName = itemName(weaponType.id).split(' ').joinToString("") { it.capitalize() }
+            println("Exporting $categoryName")
             val weapons = Items.all().filter { it.parent == weaponType.id }
             for (weapon in weapons) {
+                println("Exporting $categoryName - ${itemShortName(weapon.id)}")
                 val itemTree = weaponBuilds(weapon)
                 val weaponTree = processTree(itemTree)
-//                printJson(weaponTree)
+                Paths.get(tempDir.absolutePath, categoryName).toFile().mkdirs()
+                val file = Files.createFile(Paths.get(tempDir.absolutePath, categoryName, weaponTree.id + ".json")).toFile()
+                file.createNewFile()
+                file.writeText(stringBuilder(weaponTree))
             }
         }
+        println(tempDir.absoluteFile)
     }
 }
